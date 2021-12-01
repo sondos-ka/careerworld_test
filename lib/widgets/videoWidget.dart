055376af9fb263cf7522b9c987002b6a4cd28b89
sondos <sondoskannous@gmail.com>
2021-12-02 00:01:20 +0200

    2

--- lib/widgets/videoWidget.dart
+++ lib/widgets/videoWidget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
-import 'package:video_player/video_player.dart';
 
 class VideoWidget extends StatefulWidget {
   @override
@@ -9,20 +8,7 @@ class VideoWidget extends StatefulWidget {
 }
 
 class _VideoWidgetState extends State<VideoWidget> {
-  var controller;
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-    controller = VideoPlayerController.network(
-        'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4')
-      ..initialize().then((_) {
-        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-        setState(() {});
-      });
-  }
 @override
   Widget build(BuildContext context) {
 
